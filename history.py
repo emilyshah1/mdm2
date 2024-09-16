47749edd9b94b5357b66d7ea7753cbc3d9aa1efd
Emily Shah <ca23825@bristol.ac.uk>
2024-09-16 15:27:21 +0100

    Add all initial files

---
+++ history.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Mon Sep 16 14:58:41 2024)---
+%runfile /Users/emilyshah/.spyder-py3/temp.py --wdir
+%runfile /Users/emilyshah/.spyder-py3/temp.py --wdir
+
+## ---(Mon Sep 16 15:18:35 2024)---
+%runfile /Users/emilyshah/.spyder-py3/temp.py --wdir
---
+++ user-env.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh -i
+unset HISTFILE
+/bin/zsh -l -c "'/Users/emilyshah/Library/spyder-6/envs/spyder-runtime/bin/python' -c 'import os; print(dict(os.environ))'"
